1ed961283b430f9abcd87e9045e405b2543a955b
zane <zhouyulong@zhouyulongdeMac-mini.local>
2014-07-14 00:58:52 +0800

    增加管理权限

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def show_name
+   "#{self.email} (#{self.role_name})"
+  end
+
+  def role_name
+    return "Admin" if admin?
+    return "User"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713155420) do
+ActiveRecord::Schema.define(version: 20140714002730) do
 
   create_table "foods", force: true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140713155420) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
