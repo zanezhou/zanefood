3ac5c230c36dba842713a3cd293fba87294ea620
zane <zhouyulong@zhouyulongdeMac-mini.local>
2014-07-13 22:43:14 +0800

    上传图片、图片缩略图

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,6 @@ class Food < ActiveRecord::Base
 	validates :name, presence: true, length: {minimum: 2}
 	validates :price, presence: true, numericality: {greater_than_ir_equal_to: 0, less_than: 50}
 	validates :image, presence: true
+
+	mount_uploader :image, FoodImageUploader
 end
