a8b42ca9d774f1c8f806cde73b5a7c83dc30a382
zane <zhouyulong@zhouyulongdeMac-mini.local>
2014-07-13 21:51:20 +0800

    加入字段限制

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ActiveRecord::Base
+	validates :name, presence: true, length: {minimum: 2}
+	validates :price, presence: true, numericality: {greater_than_ir_equal_to: 0, less_than: 50}
+	validates :image, presence: true
 end
